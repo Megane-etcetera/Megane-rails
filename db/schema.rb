4ae6004822f83c1965c981408a997763cacc8b33
daizo0509 <yuya.ohkura10@gmail.com>
2018-06-15 08:39:45 +0000

    OrderModel& controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180615072323) do
+ActiveRecord::Schema.define(version: 20180615082254) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +28,18 @@ ActiveRecord::Schema.define(version: 20180615072323) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "payment"
+    t.string "address"
+    t.integer "tax"
+    t.integer "delivery_price"
+    t.integer "payment_price"
+    t.integer "order_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
