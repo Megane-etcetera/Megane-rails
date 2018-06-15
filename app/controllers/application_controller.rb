38e2ca9b79b77b09c5dee374de634dbe2f4b4385
FujitaTomo <ponrere@gmail.com>
2018-06-15 16:31:23 +0900

    first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
 
 	def after_sign_in_path_for(resource)
-	  admintops_top_path
+	  case resource
+      when 
+        root_path
+      when Admin
+        admintops_top_path
+      end
+	end
+	def after_sign_out_path_for(resource)
+	 	root_path
 	end
 	  protect_from_forgery with: :exception
 end
