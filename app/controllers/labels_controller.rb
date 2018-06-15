b7e072963679c208befbada709b4acf7e444ae82
FujitaTomo <ponrere@gmail.com>
2018-06-15 12:42:42 +0000

    修正

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,6 +1,5 @@
 class LabelsController < ApplicationController
   def index
-<<<<<<< HEAD
   	@label = Label.new
   	@labels = Label.all
   end
@@ -21,22 +20,5 @@ class LabelsController < ApplicationController
     def label_params
         params.require(:label).permit(:label)
     end
-=======
-  end
 
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
->>>>>>> 242015dbcfb18600eaae9bd97660c8c862d61a61
 end
