53fbf05ba59756b878fd393c8016835396c08575
FujitaTomo <ponrere@gmail.com>
2018-06-15 11:35:24 +0000

    error evade

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20180615083805) do
 
   create_table "labels", force: :cascade do |t|
     t.string "label"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
