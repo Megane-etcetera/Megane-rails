e91e4e464baaba6f632055320ce4dde2121cba1b
daizo0509 <yuya.ohkura10@gmail.com>
2018-06-17 12:57:20 +0000

    ContactFinishedAdd

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -14,22 +14,24 @@ class ContactsController < ApplicationController
   end
 
   def finished
-      @contact = Contact.all
+      @contacts = Contact.where(situations: params[:situations],situations: "対応済み")
+      binding.pry
   end
 
   def show
       @contact = Contact.find(params[:id])
-     
   end
 
   def update
       @contact = Contact.find(params[:id])
+      @contact.situations = params[:situationss][:name]
       @contact.update(contact_params)
+      # binding.pry
       redirect_to contacts_path
   end
 
 private
     def contact_params
-      params.require(:contact).permit(:category,:message,:admin_comment,:status)
+      params.require(:contact).permit(:category,:message,:admin_comment,:situations)
     end
 end
