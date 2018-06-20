8e0a0a11ccd21765c7375b010f0f1edc6e05645d
FujitaTomo <ponrere@gmail.com>
2018-06-20 06:57:48 +0000

    商品登録曲目登録完了

--- app/controllers/discs_controller.rb
+++ app/controllers/discs_controller.rb
@@ -20,15 +20,15 @@ class DiscsController < ApplicationController
   end
 
   def edit
-    product = Product.find(params[:product_id])
-    disc = Disc.find(params[:id])
-    disc.update
-    redirect_to new_product_disc_path(product)
+
 
   end
 
   def update
-
+    product = Product.find(params[:product_id])
+    disc = Disc.find(params[:id])
+    disc.update(disc_params)
+    redirect_to new_product_disc_path(product)
   end
 
   def destroy
@@ -36,7 +36,7 @@ class DiscsController < ApplicationController
 
   private
     def disc_params
-      params.require(:disc).permit(:product_id, :disc_type, :disc_number,tracks_attributes:[:id, :disc_id, :track_title, :track_title_kana, :track_number, :artist_name, :artist_name_kana,  :_destroy])
+      params.require(:disc).permit(:product_id, :disc_type, :disc_number, :tracks_attributes =>[:id, :disc_id, :track_title, :track_title_kana, :track_number, :artist_name, :artist_name_kana,  :_destroy])
     end
 
 end
