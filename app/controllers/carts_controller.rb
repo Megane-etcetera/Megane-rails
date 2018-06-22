86e14684dc063eeb250d16b66d3ce15f45ea4dbd
daizo0509 <yuya.ohkura10@gmail.com>
2018-06-22 12:16:14 +0000

    aaa

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,7 @@ class CartsController < ApplicationController
   def show
   	@user = current_user.id
   	@cart = current_user.carts
+  	@
   	# @product = Product.find(params[:id]) =(cart.product)
     # @discs = @product.discs
      # @artists = @discs.joins(:tracks).pluck(:artist_name).uniq
@@ -16,7 +17,7 @@ class CartsController < ApplicationController
      
       @cart.update(contact_params)
       redirect_to cart_user_path(@user)
-  end
+  end 
 
   private
   	def cart_params
