cc7eafeff848af8307112fb71b6d7d8f2b3b823a
FujitaTomo <ponrere@gmail.com>
2018-06-23 05:04:05 +0000

    プル用

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,7 @@
 class CartsController < ApplicationController
   def show
-  	@cart = current_user.carts
+  	@user = current_user.id
+  	@cart = @user.carts
   	# @product = Product.find(params[:id])
     # @discs = @product.discs
      # @artists = @discs.joins(:tracks).pluck(:artist_name).uniq
