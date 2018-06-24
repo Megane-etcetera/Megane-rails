642ca57307fe218556492c1ee6c3468650772c43
TenpuraTaiki <sleeping.3.fairies@gmail.com>
2018-06-24 19:02:14 +0900

    order_productの管理者用indexとeditを作成

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -6,17 +6,30 @@ class OrderProductsController < ApplicationController
   end
 
   def index
+    @orders = OrderProduct.all
   end
 
   def show
   end
 
   def edit
+    @order = OrderProduct.find(params[:id])
   end
 
   def update
+    @order = OrderProduct.find(params[:id])
+    @order.update(order_product_params)
+    redirect_to order_products_path
   end
 
   def destroy
   end
+end
+
+
+private
+def order_product_params
+
+  params.require(:order_product).permit(:order_id,:quantity,:product_id, :status,:order_product_price)
+
 end
