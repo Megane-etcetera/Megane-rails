a1f8c1a4b679e6fba2b021e9995d9cf39a31f5ff
TenpuraTaiki <sleeping.3.fairies@gmail.com>
2018-06-26 19:38:43 +0900

    管理者側から送料一覧の確認編集が可能に。それ用に都道府県のseedを作成。しかし実際の注文には反映してません

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,10 +1,22 @@
 class RegionsController < ApplicationController
   def index
+    @regions = Region.all
   end
 
   def edit
+    @region = Region.find(params[:id])
   end
 
   def update
+    region = Region.find(params[:id])
+    region.update(region_params)
+    redirect_to regions_path
   end
+
+  private
+    
+  def region_params
+    params.require(:region).permit(:region_name, :delivery_price)
+  end
+
 end
